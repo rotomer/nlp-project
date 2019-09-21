c5fdbfa9b7129d5c4d3e7d9c1d40631071ab4938
rotomer <rotomer9@gmail.com>
2019-09-21 22:02:04 +0300

    Some cleanups

--- src/data_frame_creator.py
+++ src/data_frame_creator.py
@@ -7,92 +7,6 @@ from src.tickers import TICKERS
 current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-#
-# def create_sentiment_8k_prediction_data(prediction_horizon_in_days):
-#     tickers = _get_list_of_tickers_from_sentiment_8k()
-#     _create_prediction_data_file(tickers, prediction_horizon_in_days)
-#
-#
-# def _get_list_of_tickers_from_sentiment_8k():
-#     sentiment_8k_dir = os.path.join(current_file_dir_path, '..', 'data', 'Sentiment_8K')
-#     file_names = os.listdir(sentiment_8k_dir)
-#
-#     return map(lambda x: os.path.splitext(x)[0], file_names)
-
-#
-# def _create_prediction_data_file(tickers, prediction_horizon_in_days):
-#     prediction_data_dir = os.path.join(current_file_dir_path, '..', 'data', 'Sentiment_8K_Prediction_Data')
-#
-#     if not os.path.exists(prediction_data_dir):
-#         os.makedirs(prediction_data_dir)
-#
-#     prediction_data_file_path = os.path.join(prediction_data_dir,
-#                                              'sentiment_8k_' + str(prediction_horizon_in_days) + '.csv')
-#     with open(prediction_data_file_path, 'w') as prediction_data_file:
-#         prediction_data_file.write('Ticker,Date,EPS_Surprise_Percentage,Days_Since_EPS,Polarity,Subjectivity,Trend\n')
-#
-#         valid_tickers_list = []
-#         for ticker in tickers:
-#             try:
-#                 print('Creating sentiment based 8k prediction data for:' + ticker + '...')
-#                 _append_rows_to_csv(prediction_data_file, ticker, prediction_horizon_in_days)
-#                 valid_tickers_list.append(ticker)
-#                 print('Done.')
-#             except FileNotFoundError:
-#                 pass
-#                 # print_exc()
-#
-#         print(str(valid_tickers_list))
-
-#
-# def _append_rows_to_csv(prediction_data_file, ticker, prediction_horizon_in_days):
-#     indexed_eps_dir = os.path.join(current_file_dir_path, '..', 'data', 'Indexed_Eps')
-#     indexed_eps_file_path = os.path.join(indexed_eps_dir, ticker + '.txt')
-#
-#     price_history_dir = os.path.join(current_file_dir_path, '..', 'data', 'price_history')
-#     price_history_file_path = os.path.join(price_history_dir, ticker + '.csv')
-#
-#     sentiment_8k_dir = os.path.join(current_file_dir_path, '..', 'data', 'Sentiment_8K')
-#     sentiment_8k_file_path = os.path.join(sentiment_8k_dir, ticker + '.csv')
-#
-#     open_prices_dict, close_prices_dict = _get_prices_for_ticker(price_history_file_path)
-#     eps_list = _get_eps_list_from_file(indexed_eps_file_path)
-#
-#     line_number = 0
-#     with open(sentiment_8k_file_path, 'r') as sentiment_8k_file:
-#         for line in sentiment_8k_file:
-#             line_number = line_number + 1
-#             if line_number == 1:
-#                 continue
-#
-#             splits = line.split(',')
-#
-#             filing_date_str = splits[0]
-#             filing_date = datetime.datetime.strptime(filing_date_str, "%Y-%m-%d").date()
-#             try:
-#                 polarity = float(splits[1])
-#                 subjectivity = float(splits[2])
-#             except ValueError:
-#                 continue
-#
-#             try:
-#                 eps_surprise_percentage, days_since_eps = _get_relevant_eps_data(filing_date, eps_list)
-#             except ValueError:
-#                 continue
-#
-#             base_price = _find_base_price(close_prices_dict, filing_date)
-#             price_at_horizon = _find_price_at_horizon(close_prices_dict, filing_date, prediction_horizon_in_days)
-#             if base_price is None or price_at_horizon is None or base_price == 0.0 or price_at_horizon == 0.0:
-#                 continue
-#
-#             price_trend_percentage = price_at_horizon / base_price
-#             price_trend = 1 if price_trend_percentage >= 1 else 0
-#
-#             prediction_data_file.write(
-#                 ticker + ',' + str(filing_date) + ',' + str(eps_surprise_percentage) + ',' + str(days_since_eps) + ','
-#                 + str(polarity) + ',' + str(subjectivity) + ',' + str(price_trend) + '\n')
-
-
 def _get_relevant_eps_data(filing_date, eps_list):
     for eps_date, eps_surprise_percentage in eps_list:
         if eps_date <= filing_date:
@@ -438,8 +352,6 @@ def _append_8k_row_for_ticker(data_frame_csv_file,
               str(price_trends[5])  # 720 days
               )
 
-    # if filing_date >= datetime.date(2005, 1, 1):
-    #     data_frame_csv_file.write(','.join(record) + '\n')
     data_frame_csv_file.write(','.join(record) + '\n')
 
     averages_8k = (
@@ -526,8 +438,6 @@ def _append_10k_row_for_ticker(data_frame_csv_file,
               str(price_trends[5])  # 720 days
               )
 
-    # if filing_date >= datetime.date(2005, 1, 1):
-    #     data_frame_csv_file.write(','.join(record) + '\n')
     data_frame_csv_file.write(','.join(record) + '\n')
 
     averages_10k = (

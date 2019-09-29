57861a8f0882c0b0e419777ee728997439d8cf76
rotomer <rotomer9@gmail.com>
2019-09-29 06:31:23 +0300

    Some cleanups

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nlp-project
 Analysis of security filings For predicting stock price trend using Relation Extraction and other information extraction techniques.
+
+This repo is the implementation of the article under the `latex` folder.
+
+**Instructions to run:**
+1. Clone the repo - https://github.com/rotomer/stanford-corenlp-wrapper and run it.
+1. run full_pipeline.full_pipeline()
+
+
--- src/full_pipeline.py
+++ src/full_pipeline.py
@@ -4,7 +4,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from statsmodels.stats.contingency_tables import mcnemar
 
+from src import filing_default_sentiment_indexer, specialized_sentiment_analysis, filing_relation_indexer
+from src.cosine_similiarity_indexer import index_cosine_similarities
+from src.data_frame_creator import create_data_frame_csv_file
 from src.gradient_boosting_predictor import GradientBoostingPredictor
+from src.management_departure_indexer import index_departures
 
 eps_feature_column_names = [
     'EncodedTicker',
@@ -87,202 +91,249 @@ target_column_name = 'Trend_1080'
 encoded_column_names = ['Ticker']
 
 
-class FullPipeline(object):
-    def __init__(self):
-        pass
+def train_test_inner_8k(horizon):
+    current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    def split(self, target_column):
-        current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
-        training_file_path = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames',
-                                          'relations_cosine_specialized_sentiment_1080.csv')
+    train_file_path_8k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
+                                      'train.csv')
+    test_file_path_8k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
+                                     'test.csv')
+    train_8k_df = pd.read_csv(train_file_path_8k)
+    test_8k_df = pd.read_csv(test_file_path_8k)
 
-        df = pd.read_csv(training_file_path)
-        y = df[target_column].values.ravel()
+    target_col_name = 'Trend_' + str(horizon)
+    predictor = GradientBoostingPredictor.create_predictor_from_training(
+        train_8k_df,
+        feature_column_names_8k,
+        target_col_name,
+        encoded_column_names=encoded_column_names)
 
-        train_df, test_df = train_test_split(df, test_size=0.2, stratified=y)
+    output_df = predictor.predict(test_8k_df, feature_column_names_8k, target_col_name)
 
-        train_df.to_csv(os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
-                                     'train_7.csv'))
+    predictor = GradientBoostingPredictor.create_predictor_from_training(
+        train_8k_df,
+        eps_feature_column_names,
+        target_col_name,
+        encoded_column_names=encoded_column_names)
 
-        test_df.to_csv(os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
-                                    'test_7.csv'))
+    output_df = predictor.predict(test_8k_df, eps_feature_column_names, target_col_name)
 
-    @staticmethod
-    def train_test_inner_8k(horizon):
-        current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
 
-        train_file_path_8k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
-                                          'train.csv')
-        test_file_path_8k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
-                                         'test.csv')
-        train_8k_df = pd.read_csv(train_file_path_8k)
-        test_8k_df = pd.read_csv(test_file_path_8k)
+def train_test_inner_10k(horizon):
+    current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
 
-        target_col_name = 'Trend_' + str(horizon)
-        predictor = GradientBoostingPredictor.create_predictor_from_training(
-            train_8k_df,
-            feature_column_names_8k,
-            target_col_name,
-            encoded_column_names=encoded_column_names)
+    train_file_path_10k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
+                                       'train.csv')
+    test_file_path_10k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
+                                      'test.csv')
+    train_10k_df = pd.read_csv(train_file_path_10k)
+    test_10k_df = pd.read_csv(test_file_path_10k)
 
-        output_df = predictor.predict(test_8k_df, feature_column_names_8k, target_col_name)
+    target_col_name = 'Trend_' + str(horizon)
+    predictor = GradientBoostingPredictor.create_predictor_from_training(
+        train_10k_df,
+        feature_column_names_10k,
+        target_col_name,
+        encoded_column_names=encoded_column_names)
 
-        predictor = GradientBoostingPredictor.create_predictor_from_training(
-            train_8k_df,
-            eps_feature_column_names,
-            target_col_name,
-            encoded_column_names=encoded_column_names)
+    output_df = predictor.predict(test_10k_df, feature_column_names_10k, target_col_name)
 
-        output_df = predictor.predict(test_8k_df, eps_feature_column_names, target_col_name)
+    predictor = GradientBoostingPredictor.create_predictor_from_training(
+        train_10k_df,
+        eps_feature_column_names,
+        target_col_name,
+        encoded_column_names=encoded_column_names)
 
-    @staticmethod
-    def train_test_inner_10k(horizon):
-        current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
+    output_df = predictor.predict(test_10k_df, eps_feature_column_names, target_col_name)
 
-        train_file_path_10k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
-                                           'train.csv')
-        test_file_path_10k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
-                                          'test.csv')
-        train_10k_df = pd.read_csv(train_file_path_10k)
-        test_10k_df = pd.read_csv(test_file_path_10k)
+def train_and_test_8k():
+    for horizon in [5, 30, 90, 180, 360, 720, 1080]:
+        print("horizon:" + str(horizon))
+        train_test_inner_8k(horizon)
+        print('----')
 
-        target_col_name = 'Trend_' + str(horizon)
-        predictor = GradientBoostingPredictor.create_predictor_from_training(
-            train_10k_df,
-            feature_column_names_10k,
-            target_col_name,
-            encoded_column_names=encoded_column_names)
+def train_and_test_10k():
+    for horizon in [5, 30, 90, 180, 360, 720, 1080]:
+        print("horizon:" + str(horizon))
+        train_test_inner_10k(horizon)
+        print('----')
 
-        output_df = predictor.predict(test_10k_df, feature_column_names_10k, target_col_name)
 
-        predictor = GradientBoostingPredictor.create_predictor_from_training(
-            train_10k_df,
-            eps_feature_column_names,
-            target_col_name,
-            encoded_column_names=encoded_column_names)
+def calc_mcnemar_8k(horizon):
+    current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
 
-        output_df = predictor.predict(test_10k_df, eps_feature_column_names, target_col_name)
+    train_file_path_8k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
+                                      'train.csv')
+    test_file_path_8k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
+                                     'test.csv')
 
-    def train_and_test_8k(self):
-        # with ProcessPoolExecutor(max_workers=8) as executor:
-        for horizon in [5, 30, 90, 180, 360, 720, 1080]:
-            print("horizon:" + str(horizon))
-            FullPipeline.train_test_inner_8k(horizon)
-            print('----')
+    test_8k_df = pd.read_csv(test_file_path_8k)
 
-    def train_and_test_10k(self):
-        # with ProcessPoolExecutor(max_workers=8) as executor:
-        for horizon in [5, 30, 90, 180, 360, 720, 1080]:
-            print("horizon:" + str(horizon))
-            FullPipeline.train_test_inner_10k(horizon)
-            print('----')
+    target_col_name = 'Trend_' + str(horizon)
 
-    @staticmethod
-    def calc_mcnemar_8k(horizon):
-        current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
+    predictor = GradientBoostingPredictor.load_predictor_from_file(
+        os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
+                     'model_ling' + str(horizon) + '.joblib'))
 
-        train_file_path_8k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
-                                          'train.csv')
-        test_file_path_8k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
-                                         'test.csv')
+    ling_output_df = predictor.predict(test_8k_df, feature_column_names_8k, target_col_name)
 
-        test_8k_df = pd.read_csv(test_file_path_8k)
+    predictor = GradientBoostingPredictor.load_predictor_from_file(
+        os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
+                     'model_eps' + str(horizon) + '.joblib'))
+    eps_output_df = predictor.predict(test_8k_df, eps_feature_column_names, target_col_name)
 
-        target_col_name = 'Trend_' + str(horizon)
+    contingency_table = calc_contingency_table(ling_output_df, eps_output_df)
+    print(str(contingency_table))
 
-        predictor = GradientBoostingPredictor.load_predictor_from_file(
-            os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
-                         'model_ling' + str(horizon) + '.joblib'))
+    result = mcnemar(contingency_table, exact=True)
+    print('statistic=%.3f, p-value=%.3f' % (result.statistic, result.pvalue))
+    alpha = 0.05
+    if result.pvalue > alpha:
+        print('Same proportions of errors (fail to reject H0)')
+    else:
+        print('Different proportions of errors (reject H0)')
 
-        ling_output_df = predictor.predict(test_8k_df, feature_column_names_8k, target_col_name)
+    print(str(contingency_table))
 
-        predictor = GradientBoostingPredictor.load_predictor_from_file(
-            os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
-                         'model_eps' + str(horizon) + '.joblib'))
-        eps_output_df = predictor.predict(test_8k_df, eps_feature_column_names, target_col_name)
 
-        contingency_table = FullPipeline.calc_contingency_table(ling_output_df, eps_output_df)
-        print(str(contingency_table))
+def calc_mcnemar():
+    for horizon in [5, 30, 90, 180, 360, 720, 1080]:
+        print("horizon:" + str(horizon))
+        calc_mcnemar_8k(horizon)
+        calc_mcnemar_10k(horizon)
+        print('----')
 
-        result = mcnemar(contingency_table, exact=True)
-        print('statistic=%.3f, p-value=%.3f' % (result.statistic, result.pvalue))
-        alpha = 0.05
-        if result.pvalue > alpha:
-            print('Same proportions of errors (fail to reject H0)')
+
+def calc_mcnemar_10k(horizon):
+    current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    train_file_path_10k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
+                                      'train.csv')
+    test_file_path_10k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
+                                     'test.csv')
+    train_10k_df = pd.read_csv(train_file_path_10k)
+    test_10k_df = pd.read_csv(test_file_path_10k)
+
+    target_col_name = 'Trend_' + str(horizon)
+
+    predictor = GradientBoostingPredictor.load_predictor_from_file(
+        os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
+                     'model_ling' + str(horizon) + '.joblib'))
+
+    ling_output_df = predictor.predict(test_10k_df, feature_column_names_10k, target_col_name)
+
+    predictor = GradientBoostingPredictor.load_predictor_from_file(
+        os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
+                     'model_eps' + str(horizon) + '.joblib'))
+    eps_output_df = predictor.predict(test_10k_df, eps_feature_column_names, target_col_name)
+
+    contingency_table = calc_contingency_table(ling_output_df, eps_output_df)
+    print(str(contingency_table))
+
+    # calculate mcnemar test
+    result = mcnemar(contingency_table, exact=True)
+    # summarize the finding
+    print('statistic=%.3f, p-value=%.3f' % (result.statistic, result.pvalue))
+    # interpret the p-value
+    alpha = 0.05
+    if result.pvalue > alpha:
+        print('Same proportions of errors (fail to reject H0)')
+    else:
+        print('Different proportions of errors (reject H0)')
+
+    print(str(contingency_table))
+
+
+def split():
+    current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
+    training_file_path = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames',
+                                      'relations_cosine_specialized_sentiment_1080.csv')
+
+    df = pd.read_csv(training_file_path)
+
+    train_df, test_df = train_test_split(df, test_size=0.2)
+
+    train_df.to_csv(os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
+                                 'train.csv'))
+
+    test_df.to_csv(os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
+                                'test.csv'))
+
+    train_df.to_csv(os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
+                                 'train.csv'))
+
+    test_df.to_csv(os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '8K',
+                                'test.csv'))
+
+
+def calc_contingency_table(ling_output_df, eps_output_df):
+    yes_ling_yes_eps = 0  # [0,0] (row, col)
+    yes_ling_no_eps = 0  # [1,0] (row, col)
+    no_ling_yes_eps = 0  # [0,1] (row, col)
+    no_ling_no_eps = 0  # [1,1] (row, col)
+
+    for index, _ in ling_output_df.iterrows():
+        if ling_output_df.iloc[index, :].loc['Predicted'] == ling_output_df.iloc[index, :].loc['Actual'] and \
+                eps_output_df.iloc[index, :].loc['Predicted'] == eps_output_df.iloc[index, :].loc['Actual']:
+            yes_ling_yes_eps += 1
+        elif ling_output_df.iloc[index, :].loc['Predicted'] == ling_output_df.iloc[index, :].loc['Actual'] and \
+                eps_output_df.iloc[index, :].loc['Predicted'] != eps_output_df.iloc[index, :].loc['Actual']:
+            yes_ling_no_eps += 1
+        elif ling_output_df.iloc[index, :].loc['Predicted'] != ling_output_df.iloc[index, :].loc['Actual'] and \
+                eps_output_df.iloc[index, :].loc['Predicted'] == eps_output_df.iloc[index, :].loc['Actual']:
+            no_ling_yes_eps += 1
         else:
-            print('Different proportions of errors (reject H0)')
+            no_ling_no_eps += 1
 
-        print(str(contingency_table))
+    return [[yes_ling_yes_eps, no_ling_yes_eps],
+            [yes_ling_no_eps, no_ling_no_eps]]
 
-    @staticmethod
-    def calc_mcnemar_10k(horizon):
-        current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
 
-        train_file_path_10k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
-                                          'train.csv')
-        test_file_path_10k = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
-                                         'test.csv')
-        train_10k_df = pd.read_csv(train_file_path_10k)
-        test_10k_df = pd.read_csv(test_file_path_10k)
+def create_data_frame():
+    current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
+    data_frame_dir = os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames')
+    data_frame_csv_file_path = os.path.join(data_frame_dir, 'relations_cosine_specialized_sentiment_1080.csv')
 
-        target_col_name = 'Trend_' + str(horizon)
+    if not os.path.exists(data_frame_dir):
+        os.makedirs(data_frame_dir)
 
-        predictor = GradientBoostingPredictor.load_predictor_from_file(
-            os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
-                         'model_ling' + str(horizon) + '.joblib'))
+    create_data_frame_csv_file(data_frame_csv_file_path)
 
-        ling_output_df = predictor.predict(test_10k_df, feature_column_names_10k, target_col_name)
 
-        predictor = GradientBoostingPredictor.load_predictor_from_file(
-            os.path.join(current_file_dir_path, '..', 'data', 'Data_Frames', '10K',
-                         'model_eps' + str(horizon) + '.joblib'))
-        eps_output_df = predictor.predict(test_10k_df, eps_feature_column_names, target_col_name)
+def index_simple_sentiment():
+    filing_default_sentiment_indexer.index_sentiment('8')
+    filing_default_sentiment_indexer.index_sentiment('10')
 
-        contingency_table = FullPipeline.calc_contingency_table(ling_output_df, eps_output_df)
-        print(str(contingency_table))
 
-        # calculate mcnemar test
-        result = mcnemar(contingency_table, exact=True)
-        # summarize the finding
-        print('statistic=%.3f, p-value=%.3f' % (result.statistic, result.pvalue))
-        # interpret the p-value
-        alpha = 0.05
-        if result.pvalue > alpha:
-            print('Same proportions of errors (fail to reject H0)')
-        else:
-            print('Different proportions of errors (reject H0)')
+def index_specialized_sentiment_analysis():
+    specialized_sentiment_analysis.index_specialized_sentiment_analysis('8')
+    specialized_sentiment_analysis.index_specialized_sentiment_analysis('10')
 
-        print(str(contingency_table))
 
-    @staticmethod
-    def calc_contingency_table(ling_output_df, eps_output_df):
-        yes_ling_yes_eps = 0  # [0,0] (row, col)
-        yes_ling_no_eps = 0  # [1,0] (row, col)
-        no_ling_yes_eps = 0  # [0,1] (row, col)
-        no_ling_no_eps = 0  # [1,1] (row, col)
+def index_relations():
+    current_file_dir_path = os.path.dirname(os.path.realpath(__file__))
+    indexed_8k_dir = os.path.join(current_file_dir_path, '..', 'input_data', 'Indexed_8K')
+    relations_8k_dir = os.path.join(current_file_dir_path, '..', 'data', 'Relations_8k')
 
-        for index, _ in ling_output_df.iterrows():
-            if ling_output_df.iloc[index, :].loc['Predicted'] == ling_output_df.iloc[index, :].loc['Actual'] and \
-                    eps_output_df.iloc[index, :].loc['Predicted'] == eps_output_df.iloc[index, :].loc['Actual']:
-                yes_ling_yes_eps += 1
-            elif ling_output_df.iloc[index, :].loc['Predicted'] == ling_output_df.iloc[index, :].loc['Actual'] and \
-                    eps_output_df.iloc[index, :].loc['Predicted'] != eps_output_df.iloc[index, :].loc['Actual']:
-                yes_ling_no_eps += 1
-            elif ling_output_df.iloc[index, :].loc['Predicted'] != ling_output_df.iloc[index, :].loc['Actual'] and \
-                    eps_output_df.iloc[index, :].loc['Predicted'] == eps_output_df.iloc[index, :].loc['Actual']:
-                no_ling_yes_eps += 1
-            else:
-                no_ling_no_eps += 1
+    filing_relation_indexer.index_relations(indexed_8k_dir, relations_8k_dir)
 
-        return [[yes_ling_yes_eps, no_ling_yes_eps],
-                [yes_ling_no_eps, no_ling_no_eps]]
+
+def full_pipeline():
+    index_simple_sentiment()
+    index_specialized_sentiment_analysis()
+    index_cosine_similarities()
+    index_relations()
+    index_departures()
+
+    create_data_frame()
+
+    split()
+
+    train_and_test_8k()
+    train_and_test_10k()
+
+    calc_mcnemar()
 
 
 if __name__ == '__main__':
-    pipeline = FullPipeline()
-    # pipeline.train_and_test_8k()
-    # pipeline.train_and_test_10k()
-    # FullPipeline.train_test_inner_8k(720)
-    # FullPipeline.train_test_inner_10k(1080)
-    FullPipeline.calc_mcnemar_8k(1080)
-    #pipeline.split(target_column_name)
+    full_pipeline()
